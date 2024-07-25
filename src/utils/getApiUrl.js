9cf0632f7ade63475315579ea3fad2575875cb1b
Dmitrii Martynov <dmitrii.martynov@deptagency.com>
2024-07-25 14:06:56 +0300

    fixes

--- src/utils/getApiUrl.js
+++ src/utils/getApiUrl.js
@@ -1,11 +1,10 @@
 import isProduction from "./isProduction";
 
-const API_LOCAL_URL = "http://localhost:8002/api";
+const API_LOCAL_URL = "http://localhost:80/api";
 const API_PROD_URL = "https://mfdp-api-martynov-dm.amvera.io/api";
 
 const getApiUrl = () => {
   console.log("isProduction", isProduction);
-
 
   return isProduction ? API_PROD_URL : API_LOCAL_URL;
 };
